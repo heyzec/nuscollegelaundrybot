5ec457a62476bb0ff348641ec29f93cb801b9da6
heyzec <61238538+heyzec@users.noreply.github.com>
2023-03-14 00:24:36 +0800

    Update for PTB v20 (async)

--- laundrybot.py
+++ laundrybot.py
@@ -6,9 +6,14 @@ import logging
 import requests
 from datetime import datetime
 import time
+import traceback
+
 from emoji import emojize
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+from telegram.ext import ApplicationBuilder
+# from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telegram.constants import ParseMode
 
 # Set up logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -35,21 +40,22 @@ def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
     return InlineKeyboardMarkup(menu)
 
 # Building emojis for every occasion
-ebluediamond = emojize(":small_blue_diamond: ", use_aliases=True)
-etick = emojize(":white_check_mark: ", use_aliases=True)
-ecross = emojize(":x: ", use_aliases=True)
-esoon = emojize(":soon: ", use_aliases=True)
+ebluediamond = emojize(":small_blue_diamond: ", language='alias')
+etick = emojize(":white_check_mark: ", language='alias')
+ecross = emojize(":x: ", language='alias')
+esoon = emojize(":soon: ", language='alias')
 
 # start command initializes: 
-def check_handler(bot, update, user_data):
+async def check_handler(update, context):
+    user_data = context.user_data
     user = update.message.from_user
     if 'pinned_level' in user_data:
-        level_status(bot, update, user_data,
+        await level_status(update, context,
                      from_pinned_level=True, new_message=True)
     else:
-        ask_level(bot, update)
+        await ask_level(update)
 
-def ask_level(bot, update):  
+async def ask_level(update):  
     level_text = "Heyyo! I am RC4's Laundry Bot. <i>As I am currently in [BETA] mode, I can only show details for Ursa floor.</i>\n\n<b>Which laundry level do you wish to check?</b>"
     level_buttons = []
     for level in LAUNDRY_LEVELS:
@@ -57,16 +63,17 @@ def ask_level(bot, update):
         data = 'set_L{}'.format(level)
         buttons = InlineKeyboardButton(text=label, callback_data=data) # data callback to set_pinned_level
         level_buttons.append(buttons)
-    update.message.reply_text(text=level_text,
+    await update.message.reply_text(text=level_text,
                               reply_markup=build_menu(level_buttons, 1),
                               parse_mode=ParseMode.HTML)
 
-def set_pinned_level(bot, update, user_data):
+async def set_pinned_level(update, context):
+    user_data = context.user_data
     query = update.callback_query
     level = int(re.match('^set_L(5|8|11|14|17)$', query.data).group(1))
     user_data['pinned_level'] = level
 
-    level_status(bot, update, user_data, from_pinned_level=True)
+    await level_status(update, context, from_pinned_level=True)
 
 # Carves the status text for each level
 def make_status_text(level_number):
@@ -75,6 +82,12 @@ def make_status_text(level_number):
 
     # Get Request to the database backend
     machine_status = requests.get(floor_url).json()
+    # machine_status = {
+    #     'washer1': 0,
+    #     'washer2': 0,
+    #     'dryer1': 0,
+    #     'dryer2': 0,
+    # }
 
     for machine_id in MACHINES_INFO:
         if machine_status[machine_id] == 0:
@@ -119,7 +132,8 @@ def make_status_menu(level_number):
 
     return build_menu(level_buttons, 5, footer_buttons=refresh_button, header_buttons=help_button)
 
-def level_status(bot, update, user_data, from_pinned_level=False, new_message=False):
+async def level_status(update, context, from_pinned_level=False, new_message=False):
+    user_data = context.user_data
     query = update.callback_query
     if from_pinned_level:
         level = user_data['pinned_level']
@@ -129,18 +143,19 @@ def level_status(bot, update, user_data, from_pinned_level=False, new_message=Fa
     user_data['check_level'] = level
 
     if new_message:
-        update.message.reply_text(text=make_status_text(level),
+        await update.message.reply_text(text=make_status_text(level),
                                   reply_markup=make_status_menu(level),
                                   parse_mode=ParseMode.HTML)
     else:
-        bot.edit_message_text(text=make_status_text(level),
+        await context.bot.edit_message_text(text=make_status_text(level),
                               chat_id=query.message.chat_id,
                               message_id=query.message.message_id,
                               reply_markup=make_status_menu(level),
                               parse_mode=ParseMode.HTML)  
 
 
-def help_menu(bot, update, user_data, from_pinned_level=False, new_message=False):
+async def help_menu(update, context, from_pinned_level=False, new_message=False):
+    user_data = context.user_data
     query = update.callback_query
     help_text = "<b>Help</b>\n\n" + "Washer 1 and Dryer 2 accept coins\n" + etick + "= Available / Job done\n" + esoon + "= Job finishing soon\n" + ecross + "= In use\n"
     help_text += "\nInformation not accurate or faced a problem? Please message @PakornUe or @Cpf05, thank you!"
@@ -153,40 +168,36 @@ def help_menu(bot, update, user_data, from_pinned_level=False, new_message=False
         callback_data='check_L{}'.format(level)
     )]
 
-    bot.edit_message_text(text=help_text,
+    await context.bot.edit_message_text(text=help_text,
                             chat_id=query.message.chat_id,
                             message_id=query.message.message_id,
                             reply_markup=build_menu(help_menu_button, 1),
                             parse_mode=ParseMode.HTML)  
 
 
-def error(bot, update, error):
+async def error(update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
+    traceback.print_exc()
 
 def main():
     TOKEN = os.environ['RC4LAUNDRYBOT_TOKEN']
+    app = ApplicationBuilder().token(TOKEN).build()
     #NAME = 'nuscollegelaundrybot'
     #PORT = os.environ.get('PORT')
+    
 
-    updater = Updater(TOKEN)
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler('start', check_handler, pass_user_data=True))
-    dp.add_handler(CallbackQueryHandler(set_pinned_level,
-                                        pattern='^set_L(5|8|11|14|17)$',
-                                        pass_user_data=True))
-    dp.add_handler(CallbackQueryHandler(level_status,
-                                        pattern='^check_L(5|8|11|14|17)$',
-                                        pass_user_data=True))
-    dp.add_handler(CallbackQueryHandler(help_menu,
-                                        pattern='Help',
-                                        pass_user_data=True))
-    dp.add_error_handler(error)
+    app.add_handler(CommandHandler('start', check_handler))
+    app.add_handler(CallbackQueryHandler(set_pinned_level,
+                                        pattern='^set_L(5|8|11|14|17)$'))
+    app.add_handler(CallbackQueryHandler(level_status,
+                                        pattern='^check_L(5|8|11|14|17)$'))
+    app.add_handler(CallbackQueryHandler(help_menu,
+                                        pattern='Help'))
+    app.add_error_handler(error)
 
     #updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
     #updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
-    updater.start_polling()
-    updater.idle()
+    app.run_polling()
 
 if __name__ == '__main__':
     main()
